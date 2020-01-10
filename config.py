1f80d41c8ccfb2f4b5a6f7ea42177c97019d77e4
Ruben <32646505+RubenPhy@users.noreply.github.com>
2020-01-10 21:59:49 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+EMAIL_ADDRESS = "email_from_where_to_sent"
+PASSWORD = "key"
+EMAIL_SEND1 = "email_to_sent"
--- scraping_news.py
+++ scraping_news.py
@@ -1,12 +1,16 @@
 import urllib.request
 from inscriptis import get_text
 import smtplib
-import config
 import time
+import sys
+sys.path.append(r'C:\Users\Ruben\Documents\Inversion\Algoritmos_inversion\News')
+
+import config
+
 
 def eliminarAcentos(cadena):
 
-    d = {    '\xc1':'A',
+    d = {'\xc1':'A',
         '\xc9':'E',
         '\xcd':'I',
         '\xd3':'O',
@@ -44,27 +48,29 @@ def eliminarAcentos(cadena):
         '\xfb':'u',
         '\xfc':'u',
         '\xe5':'a'}
-
     nueva_cadena = cadena
     for c in d.keys():
-        nueva_cadena = nueva_cadena.replace(c,d[c])
+        nueva_cadena = nueva_cadena.replace(c, d[c])
 
-    auxiliar = nueva_cadena.encode('utf-8')
     return nueva_cadena
 
-def send_email(subject,msg):
-    server = smtplib.SMTP(host = 'smtp.gmail.com:587')
+
+def send_email(subject, msg, web, key_word):
+
+    server = smtplib.SMTP(host='smtp.gmail.com:587')
     server.ehlo()
     server.starttls()
-    server.login(config.EMAIL_ADDRESS,config.PASSWORD)
-    message = "Subject: {} \n\n{}".format(subject,msg)
+    server.login(config.EMAIL_ADDRESS, config.PASSWORD)
+    message = "Subject: {} \n\n{}".format(subject, msg)
     print(message)
-    server.sendmail(config.EMAIL_ADDRESS,config.EMAIL_SEND1,message)
+    server.sendmail(config.EMAIL_ADDRESS, config.EMAIL_SEND1, message)
 
     server.close()
-    print("Successfuly sent")
+    print("Successfully sent the email for the:", web, "with the word:", key_word, '. To the mail')
 
-def obtain_news(url,key_word):
+
+def get_news(url, key_word):
+
     html = urllib.request.urlopen(url).read().decode('utf-8')
     text = get_text(html)
     texts = text.split("\n\n")
@@ -75,32 +81,46 @@ def obtain_news(url,key_word):
             news.append(div)
     return news
 
-def main():
-    url = "https://www.eleconomista.es/"
-    key_word = "SUV"
 
-    news = obtain_news(url,key_word)
-    subject = 'Pyton has found the following news:'
-    msg = "\n\n".join(news)
-    msg = eliminarAcentos(msg)
+def check_and_send(f, msg, subject, web, key_word):
 
-    f = open('last_news.txt','r')
-    if f.read() != msg and msg:
-        send_email(subject,msg)
-        f.close()
-        f = open('last_news.txt','w')
+    if msg == '':
+        print("Nothing found for the web:", web, ' for the word:', key_word)
+        return True
+    elif f.read() == msg:
+        print("Message already sent for the web:", web, ' for the word:', key_word)
+    elif f.read() != msg:
+        send_email(subject, msg, web, key_word)
         f.write(msg)
         f.close()
-    elif not msg:
-        print("Nothing found")
-    
+
+
+def main():
+    urls = ["https://www.eleconomista.es/", 'https://www.elconfidencial.com/']
+    key_words = ["PSOE"]
+
+    for key_word in key_words:
+        for url in urls:
+
+            news = get_news(url, key_word)
+            subject = 'Python has found the following news:'
+            msg = "\n\n".join(news)
+            msg = eliminarAcentos(msg)
+
+            web = url.split('www.')[1].split('.')[0]
+            print('Web :', web, ' for the word:', key_word)
+            file = web + '_' + key_word + '_last_news.txt'
+
+            try:
+                with open(file, 'r+') as f:
+                    check_and_send(f, msg, subject, web, key_word)
+            except IOError:
+                with open(file, 'w'):
+                    pass
+                with open(file, 'r+') as f:
+                    check_and_send(f, msg, subject, web, key_word)
+
+
 while True:
     main()
-    time.sleep(20)
-    
-            
-        
-        
-
-
-        
+    time.sleep(30)
